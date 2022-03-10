e7aa5ee4b62733e4cb89ef2439d7b988af35f94b
MatheoMONSTER <81566430+MatheoMONSTER@users.noreply.github.com>
2022-03-11 00:00:55 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp124
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b; 
+        }
+        public int Substract(int a, int b)
+        {
+          /*  if (a > b)
+            {
+                return a - b;
+            }
+          */
+            return a-b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b; 
+        }
+        public double Divide(double a, double b)
+        {
+            if(b == 0)
+            {
+                Console.WriteLine("Cannot divide by 0!");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Calculator calc = new Calculator();
+                Console.WriteLine(calc.Add(5, 6)); //11
+                Console.WriteLine(calc.Substract(-2, 2)); //-4
+                Console.WriteLine(calc.Multiply(2, 5)); //10
+                Console.WriteLine(calc.Divide(1, 3)); //2
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
